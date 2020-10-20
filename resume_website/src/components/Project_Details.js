53d4041e4d2fb7acf2aaf16c6e17872bb7358332
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2020-10-19 19:29:58 -0500

    Added images of new project
    Setting up images to be displayed with other projects

--- resume_website/src/components/Project_Details.js
+++ resume_website/src/components/Project_Details.js
@@ -3,8 +3,42 @@ function importAll(r) {
   }
   
   const images = importAll(require.context('../assets/images', false, /\.(png|jpe?g|svg)$/));
+  const imagesFactory = importAll(require.context('../assets/images/factory', false, /\.(png|jpe?g|svg)$/));
 
 const projectDetails = [
+    {
+        title: 'TheFactory',
+        url: 'https://github.com/ColbPeyton/practice_business',
+        cardImage: imagesFactory[0],
+        imageGallery: [
+            imagesFactory[1],
+            imagesFactory[2],
+            imagesFactory[3],
+            imagesFactory[4],
+            imagesFactory[5],
+            imagesFactory[6],
+            imagesFactory[7],
+            imagesFactory[8],
+            imagesFactory[9],
+            imagesFactory[10],
+            imagesFactory[11],
+            imagesFactory[12],
+            imagesFactory[13],
+            imagesFactory[14],
+            imagesFactory[15],
+            imagesFactory[16],
+            imagesFactory[17],
+            imagesFactory[18],
+            imagesFactory[19],
+            imagesFactory[20],
+            imagesFactory[21],
+            imagesFactory[22],
+            imagesFactory[23],
+            imagesFactory[24],
+            imagesFactory[25],
+            imagesFactory[26],
+                    ]
+    },
     {
         title: 'LineTracker',
         url: 'https://github.com/jonathon-evans/CSC424-Project',
